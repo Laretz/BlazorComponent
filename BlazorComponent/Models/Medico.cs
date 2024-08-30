491e4691b10098b7c98f6e290bae3610bb4ed030
Renato Ramon <renatophoenix123@gmail.com>
2024-08-30 01:59:49 -0300

    Add project files.

---
+++ BlazorComponent/Models/Medico.cs
@@ -0,0 +1,14 @@
+﻿using BlazorComponent.Repositories;
+
+namespace BlazorComponent.Models
+{
+    public class Medico  
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Crm { get; set; }
+
+      
+     }
+    
+}
---
+++ BlazorComponent/Repositories/MedicoRepository.cs
@@ -0,0 +1,20 @@
+﻿using BlazorComponent.Models;
+
+namespace BlazorComponent.Repositories
+{
+    public class MedicoRepository : IMedicoRepository
+    {
+        public async Task<List<Medico>> GetAllAsync()
+        {
+           
+            return new List<Medico>
+            { 
+                new Medico { Id = 1, Name = "Dr. João Silva", Crm = "123456" },
+                new Medico { Id = 2, Name = "Dra. Maria Oliveira", Crm = "234567" },
+                new Medico { Id = 3, Name = "Dr. Pedro Costa", Crm = "345678" },
+                new Medico { Id = 4, Name = "Dra. Ana Souza", Crm = "456789" },
+                new Medico { Id = 5, Name = "Dr. Lucas Santos", Crm = "567890" }
+            };
+        }
+    }
+}
