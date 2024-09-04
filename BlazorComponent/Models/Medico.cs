33402e75173e5c3a70435d501836c788c309b43f
Renato Ramon <renatophoenix123@gmail.com>
2024-09-04 01:54:20 -0300

    feat: Add validation to Medicos form and handle form submission
    - Added `DataAnnotations` to the Medicos model for field validation (`[Required]`, `[MaxLength]`).
    - Implemented `ValidationMessage` components to display validation errors in the form.
    - Created `OnValidSubmit` method to handle valid form submissions and navigate to `/Medicos`.

--- BlazorComponent/Models/Medico.cs
+++ BlazorComponent/Models/Medico.cs
@@ -1,14 +1,22 @@
 ﻿using BlazorComponent.Repositories;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazorComponent.Models
 {
     public class Medico  
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="Name required")]
+        [MaxLength(50, ErrorMessage ="Name must be no longer than 50 digits")]
+        [MinLength(3, ErrorMessage = "Name must contain at least 3 digits")]
         public string? Name { get; set; }
+
+        [Required(ErrorMessage ="CRM required")]
         public string? Crm { get; set; }
 
       
      }
     
 }
+ 
